dc8a2d46dc8fd7f1e4ebcd300be8993f9f4075f3
Phil Larson <phil@mobilecoin.com>
2023-04-24 20:37:39 -0700

    macOS project

---
+++ macOS/MobyOBS/MobyOBS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MobyOBS
+//
+//  Created by Phil Larson on 4/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ macOS/MobyOBS/MobyOBS/MobyOBSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobyOBSApp.swift
+//  MobyOBS
+//
+//  Created by Phil Larson on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct MobyOBSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
